5bf09a8682cd57de2a5a402db072226d94c53a4c
diekrz2 <162576315+diekrz2@users.noreply.github.com>
2026-04-06 12:54:51 +0200

    Show progress with percentage

--- min256-finder.py
+++ min256-finder.py
@@ -1,5 +1,6 @@
 # Licensed under the MIT License
 
+import os
 import hashlib
 import tkinter as tk
 from tkinter import filedialog, messagebox
@@ -8,15 +9,34 @@ def read_block(file, block_size=4096):
     return file.read(block_size)
 
 def find_sha(file_path):
-    find_hash = hashlib.sha256()
+    sha256 = hashlib.sha256()
+    
+    total_size = os.path.getsize(file_path)
+    read_size = 0
+
     try:
+        last_percent = -1
         with open(file_path, "rb") as f:
             while True:
-                byte_block = f.read(4096)
-                if not byte_block:
+                data = f.read(4096)
+                if not data:
                     break
-                find_hash.update(byte_block)
-        return find_hash.hexdigest()
+                
+                sha256.update(data)
+                read_size += len(data)
+
+                # Simple percentage calculation
+                percent = int((read_size / total_size) * 100)
+                
+                if percent != last_percent:
+                   result_label.config(text=f"Calculating... {percent}%")
+   
+                # GUI update
+                root.update_idletasks()
+                last_percent = percent
+                
+        return sha256.hexdigest()
+
     except Exception as e:
         return f"Error: {str(e)}"
 
@@ -25,13 +45,26 @@ def select_file():
         title="Select an ISO file",
         filetypes=[("ISO", "*.iso"), ("All files", "*.*")]
     )
-    
+
     if file_path:
-        messagebox.showinfo("Info", "Calculating...")
-        hash_value = find_sha(file_path)
-        result_label.config(text=f"SHA256:\n\n{hash_value}")
+        select_button.config(state="disabled")
+        result_label.config(text="Calculating... 0%")
+        root.update_idletasks()
+
+        # Restart calculating after GUI update
+        root.after(100, lambda: start_calculation(file_path))
     else:
-        messagebox.showinfo("Info", "No file selected.")
+        result_label.config(text="No file selected.")
+        
+def start_calculation(file_path):
+    hash_value = find_sha(file_path)
+
+    select_button.config(state="normal")
+
+    # Show result
+    result_label.config(text=f"SHA256:\n\n{hash_value}")
+    # Final popup 
+    messagebox.showinfo("Info", "Done.")
 
 # Main window 
 root = tk.Tk()
